b7197867410e7338a22e5791e77cbd91f49210c3
ALICE CAMPOS DA SILVA <25203690@cfp127aluno.local>
2026-03-24 16:58:44 -0300

    Exercicio tabuada iniciando

---
+++ 06-tabuada/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function gerarAdi(Number){
+    let listaAdi = []
+    for (let  i = 1; i <= quantidade; i++){
+    gerarAdi.push(n + n)
+    }
+    return listaAdi
+    }
+
+function gerarSub(){
+}
+
+function gerarMult(){
+}
+
+function gerarDivi(){
+}
+
+console.log(gerarAdi)
